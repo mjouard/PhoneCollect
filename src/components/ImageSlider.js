92c298ff31ea7d21a50ca4b1b67858faba833c8b
mjouard <maxime.jouard@gmail.com>
2022-11-23 16:04:08 +0100

    Swipe image téléhpone (test)

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -49,8 +49,36 @@ const dotStyle = {
   fontSize: "20px",
 };
 
+
+
 const ImageSlider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState(null)
+  const [touchEnd, setTouchEnd] = useState(null)
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 10 
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return
+    const distance = touchStart - touchEnd
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+    if (isLeftSwipe) {
+      goToPrevious()
+    }
+    else if (isRightSwipe){
+      goToNext()
+    }
+    // add your conditional logic here
+  }
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
@@ -79,7 +107,7 @@ const ImageSlider = ({ slides }) => {
         </div>
       </div>
       <div style={slideStylesWidthBackground}>
-        <img src={slides[currentIndex]} alt="" style={slideStylesWidthBackground}/>
+        <img onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} src={slides[currentIndex]} alt="" style={slideStylesWidthBackground}/>
       </div>
       <div style={dotsContainerStyles}>
         {slides.map((slide, slideIndex) => (
