a17910f7fc399d87f02fab8213e40402ed7dd857
mjouard <maxime.jouard@gmail.com>
2022-12-08 14:45:01 +0100

    Home page add backgrounds

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,11 +4,16 @@ import "./page.css"
 export default function Page(props){
     return(
         <div>
-            <Header />
-            <div>
-                <img src="page/home-image-large.jpeg" className="home-image-large"/>
-                <img src="page/home-image-middle.jpeg" className="home-image-middle"/>
+            <div className="home-div-page-1">
+                <img className="home-image-large" src="page/circuit-elec-large.jpeg" />
+                <img className="home-image-middle" src="page/ampoule-middle.jpeg" />
+                <img className="home-image-small" src="page/ampoule-small.jpeg" />
+                <p className="text-home-page-1">
+                    Phone Collect
+                </p>
             </div>
+            <div className="home-div-page-2" />
+            <div className="home-div-page-3" />
         </div>
     )
 }
