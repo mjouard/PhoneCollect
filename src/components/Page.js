8463fb63eeed499dfb8f426d5a323ee9095b0521
mjouard <maxime.jouard@gmail.com>
2022-12-12 16:32:30 +0100

    bug scroll phone

--- src/components/Page.js
+++ src/components/Page.js
@@ -10,7 +10,7 @@ export default function Page(props){
       navigate('/products');
     };
 
-    function scroll(e, minScroll){
+    function wheel(e, minScroll){
         var pageHeight = window.innerHeight;
         const scrollY = window.scrollY
         const actualPage = Math.round(scrollY/pageHeight)
@@ -28,16 +28,25 @@ export default function Page(props){
         }
     }
 
+    function scroll(e, minScroll){
+        var pageHeight = window.innerHeight;
+        const scrollY = window.scrollY
+        const actualPage = Math.round(scrollY/pageHeight)
+        window.scrollTo({
+            top: pageHeight * (actualPage + 1),
+            behavior: 'smooth',
+        });
+    }
+
     const phoneSlides = ["home-2/phone-product-1.jpg", "home-2/phone-product-2.webp"]
 
     const computerSlides = ["home-2/computer-product-1.jpeg", "home-2/computer-product-2.jpg", "home-2/computer-product-3.jpeg"]
 
 
     return(
-        <div onWheel={(e)=>scroll(e, 50)} onScroll={(e) => scroll(e, 5)}>
+        <div onWheel={(e)=>wheel(e, 50)} onScroll={(e) => scroll(e, 5)}>
             <div className="home-div-page-1">
                 <p className="text-home-page-1">
-                    <img src="Iphone 12 2.png" width="500px"/>
                     Phone Collect
                 </p>
             </div>
