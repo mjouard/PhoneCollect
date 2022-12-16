36bda73a7337ccb4c18fbb56934940b058f136c0
mjouard <maxime.jouard@gmail.com>
2022-12-16 13:20:31 +0100

    colors home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,41 +10,13 @@ export default function HomePage(props){
       navigate('/products/' + type);
     };
 
-    function wheel(e, minScroll){
-        var pageHeight = window.innerHeight;
-        const scrollY = window.scrollY
-        const actualPage = Math.round(scrollY/pageHeight)
-        if (e.deltaY > minScroll){
-            window.scrollTo({
-                top: pageHeight * (actualPage + 1),
-                behavior: 'smooth',
-            });
-        }
-        else if (e.deltaY < -minScroll){
-            window.scrollTo({
-                top: pageHeight * (actualPage - 1),
-                behavior: 'smooth',
-            });
-        }
-    }
-
-    function scroll(e, minScroll){
-        var pageHeight = window.innerHeight;
-        const scrollY = window.scrollY
-        const actualPage = Math.round(scrollY/pageHeight)
-        window.scrollTo({
-            top: pageHeight * (actualPage + 1),
-            behavior: 'smooth',
-        });
-    }
-
     const phoneSlides = ["home-2/phone-product-1.jpg", "home-2/phone-product-2.webp"]
 
     const computerSlides = ["home-2/computer-product-1.jpeg", "home-2/computer-product-2.jpg", "home-2/computer-product-3.jpeg"]
 
 
     return(
-        <div onWheel={(e)=>wheel(e, 50)} onScroll={(e) => scroll(e, 5)}>
+        <div>
             <div className="home-div-page-1">
                 <p className="text-home-page-1">
                     Phone Collect
